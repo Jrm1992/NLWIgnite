326fd151c9b26cd482df82778710239873dc6d2e
Jose <jose_roberto_6@msn.com>
2022-09-15 06:18:07 -0300

    aula 04

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
       backgroundImage:{
         background: "url('/background.png')",
         'nlw-gradient': ' linear-gradient(to right, #9572FC, #43E7AD, #E1D55D)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
