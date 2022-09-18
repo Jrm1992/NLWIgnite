306d3860ec3724f9aef65efea38837c7b0f6b888
Jose <jose_roberto_6@msn.com>
2022-09-17 21:36:57 -0300

    aula 05

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,19 +1,57 @@
-import * as Dialog from '@radix-ui/react-dialog'
-import { Input } from './Form/Input'
-import { GameController } from 'phosphor-react'
+import * as Dialog from "@radix-ui/react-dialog";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import * as Select from "@radix-ui/react-select";
+import { Input } from "./Form/Input";
+import { Check, GameController, ArrowDown } from "phosphor-react";
+import { useEffect, useState } from "react";
+
+interface Games {
+  id: string
+  title: string
+}
 
 export default function CreateAdModal() {
-  return (
-  <Dialog.Portal>
-    <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+  const [games, setGames] = useState<Games[]>([])
+  const [weekDays, setWeekDays] = useState<String[]>([])
 
-    <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
-      <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+  
+
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+        <Dialog.Title className="text-3xl font-black">
+          Publique um anúncio
+        </Dialog.Title>
 
         <form className="mt-8 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
-            <label htmlFor="game" className="font-semibold">Qual o game?</label>
-            <Input id="game" placeholder="Selecione o game que deseja jogar" />
+            <label htmlFor="game" className="font-semibold">
+              Qual o game?
+            </label>
+            <select
+              id="game"
+              className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none"
+              defaultValue=""
+            >
+              <option disabled value="">Selecione o game que deseja jogar</option>
+
+              {games.map((game) => {
+                return (
+                  <option key={game.id} value={game.title}>{game.title}</option>
+                )
+              })}
+
+            </select>
           </div>
 
           <div className="flex flex-col gap-2">
@@ -24,7 +62,11 @@ export default function CreateAdModal() {
           <div className="grid grid-cols-2 gap-6">
             <div className="flex flex-col gap-2">
               <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
-              <Input id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO" />
+              <Input
+                id="yearsPlaying"
+                type="number"
+                placeholder="Tudo bem ser ZERO"
+              />
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="discord">Qual o seu Discord?</label>
@@ -36,50 +78,57 @@ export default function CreateAdModal() {
             <div className="flex flex-col gap-2">
               <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-              <div className="flex gap-1">
-                <button
-                  title="Domingo"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  D
-                </button>
-                <button
-                  title="Segunda"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  S
-                </button>
-                <button
-                  title="Terça"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  T
-                </button>
-                <button
-                  title="Quarta"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  Q
-                </button>
-                <button
-                  title="Quinta"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  Q
-                </button>
-                <button
-                  title="Sexta"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  S
-                </button>
-                <button
-                  title="Sábado"
-                  className="w-6 h-6 rounded bg-zinc-900"
-                >
-                  S
-                </button>
-              </div>
+                <ToggleGroup.Root type="multiple" className="flex gap-1" onValueChange={setWeekDays}>
+                  <ToggleGroup.Item 
+                    value="0" 
+                    title="Domingo" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('0') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    D
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="1" 
+                    title="Segunda" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('1') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    S
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="2" 
+                    title="Terça" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('2') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    T
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="3" 
+                    title="Quarta" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('3') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    Q
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="4" 
+                    title="Quinta" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('4') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    Q
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="5" 
+                    title="Sexta" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('5') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    S
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="6" 
+                    title="Sábado" 
+                    className={`w-6 h-6 rounded ${weekDays.includes('6') ? 'bg-violet-500' : 'bg-zinc-900'}`}
+                  >
+                    S
+                  </ToggleGroup.Item>
+                </ToggleGroup.Root>
             </div>
             <div className="flex flex-col gap-2 flex-1">
               <label htmlFor="hourStart">Qual horário do dia?</label>
@@ -90,10 +139,14 @@ export default function CreateAdModal() {
             </div>
           </div>
 
-          <div className="mt-2 flex gap-2 text-sm">
-            <Input type="checkbox" />
+          <label className="mt-2 flex items-center gap-2 text-sm">
+            <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+              <Checkbox.Indicator>
+                <Check className="w-4 h-4 text-emerald-400" />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
             Costumo me conectar ao chat de voz
-          </div>
+          </label>
 
           <footer className="mt-4 flex justify-end gap-4">
             <Dialog.Close
@@ -111,7 +164,7 @@ export default function CreateAdModal() {
             </button>
           </footer>
         </form>
-    </Dialog.Content>
-  </Dialog.Portal>
-  )
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
 }
